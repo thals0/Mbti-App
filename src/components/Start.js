37e5a01b292a3f6468bf06956fff95c5bbae8068
SoMin <thals0107@naver.com>
2022-10-12 17:21:07 +0900

    Create Start page component

---
+++ src/components/Start.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import OrangeButton from './OrangeButton';
+import Button from './Button';
+
+const Header = styled.p`
+  font-size: 3em;
+`;
+
+const MainImg = styled.img`
+  width: inherit;
+`;
+
+const SubHeader = styled.p`
+  font-size: 1.5em;
+  color: #777;
+`;
+
+export default function Start() {
+  return (
+    <>
+      <Header>개발자 MBTI 조사</Header>
+      <MainImg src="/images/main.jpg" alt="main img" />
+      <SubHeader>
+        개발자가 흔히 접하는 상황에 따라서 MBTI를 알아 봅시다!
+      </SubHeader>
+      <Button
+        text="테스트 시작"
+        mainColor="#fae243"
+        subColor="#fa9f1a"
+        hoverColor="#faf000"
+      />
+    </>
+  );
+}
