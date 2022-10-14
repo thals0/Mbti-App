7fb66dcd67d16e9140f7fad35870d222d78191a0
SoMin <thals0107@naver.com>
2022-10-14 14:42:00 +0900

    add check

--- src/components/Mbti.js
+++ src/components/Mbti.js
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import SkyblueButton from './SkyblueButton';
-import { next } from '../store/modules/mbti';
+import { next, check } from '../store/modules/mbti';
 import Progress from './Progress';
 
 const SurveyQuestion = styled.p`
@@ -27,7 +27,10 @@ export default function Mbti() {
             <li key={index}>
               <SkyblueButton
                 text={el.text}
-                clickEvent={() => dispatch(next())}
+                clickEvent={() => {
+                  dispatch(check(el.result));
+                  dispatch(next());
+                }}
               />
               {index === 0 && <Vs>VS</Vs>}
             </li>
