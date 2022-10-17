c1256c45d37ffa2d0dbf34dddd7dc25d6960d2a0
SoMin <thals0107@naver.com>
2022-10-17 18:11:35 +0900

    Add mongo router

--- data-server/server.js
+++ data-server/server.js
@@ -9,8 +9,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+// mysql router
 const dataRouter = require('./routes/data');
 app.use('/data', dataRouter);
+
+// mongo router
+const mongoRouter = require('./routes/mongo');
+app.use('/mongo', mongoRouter);
 
 app.listen(PORT, () => {
   console.log(`데이터 통신 서버가 ${PORT}에서 작동 중입니다. `);
