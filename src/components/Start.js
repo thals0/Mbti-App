c17aa3e5c904b108b9ea1ddef04efa9680f37dd5
SoMin <thals0107@naver.com>
2022-10-17 17:14:25 +0900

    mysql 연결, data 가공

--- src/components/Start.js
+++ src/components/Start.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import OrangeButton from './OrangeButton';
 import Button from './Button';
 import { useDispatch } from 'react-redux';
-import { next } from '../store/modules/mbti';
+import { next, init } from '../store/modules/mbti';
 import { useEffect, useState } from 'react';
 
 const Header = styled.p`
@@ -21,9 +21,38 @@ const SubHeader = styled.p`
 export default function Start() {
   const [counts, setCounts] = useState(0);
 
+  function makeData(survey, explanation) {
+    const initData = { survey: [], explanation: {} };
+    if (initData.survey.length === 0) {
+      for (let i = 0; i < survey.length; i = i + 2) {
+        initData.survey.push({
+          question: survey[i].QUESTION_TEXT,
+          answer: [
+            {
+              text: survey[i].ANSWER_TEXT,
+              result: survey[i].RESULT,
+            },
+            {
+              text: survey[i + 1].ANSWER_TEXT,
+              result: survey[i + 1].RESULT,
+            },
+          ],
+        });
+      }
+      for (let i = 0; i < explanation.length; i++) {
+        initData.explanation[explanation[i].MBTI_TYPE] = {
+          explanation: explanation[i].EXPLAINATION,
+          img: explanation[i].IMG_SRC,
+        };
+      }
+    }
+    return initData;
+  }
+
   // counts가 변경될 때 마다 실행
   useEffect(() => {
     async function fetchData() {
+      // Counts 값 받아오기
       const resCount = await fetch('http://localhost:3001/data/counts');
       // 통신성공 = 200
       if (resCount.status === 200) {
@@ -33,9 +62,36 @@ export default function Start() {
       } else {
         throw new Error('통신 이상');
       }
+
+      // Survey 값 받아오기
+      const resSurvey = await fetch('http://localhost:3001/data/survey');
+      if (resSurvey.status === 200) {
+        // json -> js data로
+        const surveyData = await resSurvey.json();
+        // console.log(surveyData);
+
+        // surveyData 값이 유지된채로 explanation 값을 받아오기 위해서 이 위치에 작성
+        // Explanation 값 받아오기
+        const resExplanation = await fetch(
+          'http://localhost:3001/data/explanation'
+        );
+        if (resExplanation.status === 200) {
+          // json -> js data로
+          const explanationData = await resExplanation.json();
+          // console.log('설명 데이터', explanationData);
+          // console.log(makeData(surveyData, explanationData));
+          const madeData = makeData(surveyData, explanationData);
+          // console.log(madeData);
+          dispatch(init(madeData));
+        } else {
+          throw new Error('통신 이상');
+        }
+      } else {
+        throw new Error('통신 이상');
+      }
     }
     fetchData();
-  }, [counts]);
+  }, []);
 
   const dispatch = useDispatch();
   return (
