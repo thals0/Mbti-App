d25ee7222e416bbc22adf6c563606d43bd83306a
SoMin <thals0107@naver.com>
2022-10-17 17:13:33 +0900

    Add router (/survey, /explanation)

--- data-server/routes/data.js
+++ data-server/routes/data.js
@@ -16,4 +16,18 @@ router.post('/inccounts', (req, res) => {
   });
 });
 
+router.get('/survey', (req, res) => {
+  db.getSurvey((data) => {
+    // console.log(data);
+    res.send(data);
+  });
+});
+
+router.get('/explanation', (req, res) => {
+  db.getExplanation((data) => {
+    // console.log('설명 데이터', data);
+    res.send(data);
+  });
+});
+
 module.exports = router;
