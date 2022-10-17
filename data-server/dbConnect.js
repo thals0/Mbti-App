e0447444a67f8a4fdb7fe78a7397d462d4a57a74
SoMin <thals0107@naver.com>
2022-10-17 17:12:31 +0900

    mysql error 해결

--- data-server/dbConnect.js
+++ data-server/dbConnect.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  // host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: 'dlthals17',
   port: '3306',
