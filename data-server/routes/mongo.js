4ba0708b8eaa710329921de1edd19945bfd9fe9f
SoMin <thals0107@naver.com>
2022-10-18 23:17:04 +0900

    Update mongo.js router 추가

--- data-server/routes/mongo.js
+++ data-server/routes/mongo.js
@@ -3,9 +3,24 @@ const router = express.Router();
 
 const mongoDB = require('../controllers/mongoController');
 
-router.post('/setData', async (req, res) => {
+router.post('/setdata', async (req, res) => {
   const msg = await mongoDB.setData();
   res.send(JSON.stringify(msg));
+});
+
+router.get('/counts', async (req, res) => {
+  const counts = await mongoDB.getCounts();
+  res.send(counts);
+});
+
+router.post('/inccounts', async (req, res) => {
+  const msg = await mongoDB.incCounts();
+  res.send(msg);
+});
+
+router.get('/getdata', async (req, res) => {
+  const data = await mongoDB.getData();
+  res.send(data);
 });
 
 module.exports = router;
