e0aa49b58f6ea3df5b0e24b6ade921a3557f86d4
SoMin <thals0107@naver.com>
2022-10-18 23:15:55 +0900

    Create mongo controller

--- data-server/controllers/mongoController.js
+++ data-server/controllers/mongoController.js
@@ -144,6 +144,28 @@ const mongoDB = {
       throw new Error('통신 이상');
     }
   },
+  getCounts: async () => {
+    const client = await _client;
+    const db = client.db('mbti').collection('counts');
+    const data = await db.find({}).toArray();
+    return data;
+  },
+  incCounts: async () => {
+    const client = await _client;
+    const db = client.db('mbti').collection('counts');
+    const result = await db.updateOne({ id: 1 }, { $inc: { counts: +1 } });
+    if (result.acknowledged) {
+      return '업데이트 성공';
+    } else {
+      throw new Error('통신 이상');
+    }
+  },
+  getData: async () => {
+    const client = await _client;
+    const db = client.db('mbti').collection('data');
+    const data = await db.find({}).toArray();
+    return data;
+  },
 };
 
 module.exports = mongoDB;
